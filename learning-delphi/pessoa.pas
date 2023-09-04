1649b6c457fe7c1c71ab966ad53b84c3ea878992
Vitória de Barros Carvalho <101741597+vitoriaacarvalho@users.noreply.github.com>
2023-09-04 11:16:11 -0300

    Add files via upload

---
+++ learning-delphi/pessoa.pas
@@ -0,0 +1,17 @@
+unit pessoa;
+
+interface
+type
+  //aqui crio: a classe:
+  TPessoa = class
+  private
+  //variaveis privadas:
+    Cpf : String;
+  public
+  //variaveis publicas:
+    Nome : String;
+
+  end;
+implementation
+
+end.
